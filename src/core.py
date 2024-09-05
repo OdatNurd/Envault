d8ccf7084cbb1ecfe5f44d3528cb2baae50b1e5a
Terence Martin <odatnurd@gmail.com>
2024-09-04 23:26:44 -0500

    On plugin load, fetch all configs currently in use
    When the plugin loads (which includes at Sublime startup), scan over all
    of the windows that were restored to see if they have set within them a
    currently selected Envault configuration file.
    
    For any that do, kick off a request to get the variables for that
    config, so that upon restart of Sublime all of the required data to keep
    tooling work is available.

--- src/core.py
+++ src/core.py
@@ -2,6 +2,26 @@ import sublime
 
 from ..lib.bootstrap import bootstrap_legacy_package
 
+from .config_loader import load_and_fetch_config
+from .envault_data import get_envault_config
+from .logging import log
+
+
+## ----------------------------------------------------------------------------
+
+
+def load_window_configs():
+    """
+    Scan over all windows, and for each one check to see if there is a current
+    envault configuration file specified. If there is, then dispatch a request
+    to fetch that configuration.
+    """
+    for window in sublime.windows():
+        config = get_envault_config(window)
+        if config:
+            log(f"doing startup fetch for {config}")
+            load_and_fetch_config(config, window)
+
 
 ## ----------------------------------------------------------------------------
 
@@ -12,9 +32,12 @@ def loaded():
     when the package starts, which in our case here is to bootstrap out the
     command that is responsible for adjusting the environment into the Python
     3.3 plugin host so that we can support build targets from both versions of
-    Sublime the same.
+    Sublime the same, and ensure that any restored windows that had envault
+    configs have that config loaded.
     """
+    log("initializing")
     bootstrap_legacy_package()
+    load_window_configs()
 
 
 def unloaded():
