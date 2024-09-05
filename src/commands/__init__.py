91b5bb625421c2aadba88817cf8a4f83fc561d77
Terence Martin <odatnurd@gmail.com>
2024-09-05 00:00:03 -0500

    Include a command to reload the active config
    Although it is possible to reload the current config by just selecting
    it again from the list, in cases where there is more than one config
    present it is handy to be able to reload it directly, since there is no
    direct way to know which one is currently active.
    
    This implements such a command, which is only available in windows that
    have a currently selected configuration.

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,9 +1,10 @@
 from ...envault import reload
 
-reload("src.commands", ["env_command","choose_config"])
+reload("src.commands", ["env_command","choose_config", "reload_config"])
 
 from .env_command import EnvaultEnvironmentCommand
 from .choose_config import EnvaultChooseConfigCommand
+from .reload_config import EnvaultReloadConfigCommand
 
 __all__ = [
     # Command that adjusts the environment for us
@@ -12,4 +13,5 @@ __all__ = [
     # Commands that scan form, load, and select the current configuration
     # file
     "EnvaultChooseConfigCommand",
+    "EnvaultReloadConfigCommand",
 ]
---
+++ src/commands/reload_config.py
@@ -0,0 +1,34 @@
+import sublime
+import sublime_plugin
+
+from ..envault_data import get_envault_config
+from ..config_loader import load_and_fetch_config
+from ..logging import log
+
+
+## ----------------------------------------------------------------------------
+
+
+class EnvaultReloadConfigCommand(sublime_plugin.WindowCommand):
+    """
+    If the current window has an Envault configuration file already selected
+    within it, reload the config from disk and re-fetch the appropriate
+    environment to go with it.
+
+    The command will disable itself when the current window does not have a
+    selected configuration.
+    """
+    def run(self):
+        # Get the current configuration file that's in use in the window, and
+        # reload it
+        config = get_envault_config(self.window)
+        log(f"reloading envault config {config}")
+        load_and_fetch_config(config, self.window)
+
+
+    def is_enabled(self):
+        return get_envault_config(self.window) != ''
+
+
+## ----------------------------------------------------------------------------
+
