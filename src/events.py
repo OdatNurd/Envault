dd6f64c6ddca747a6f45c09573b09d60e8f70ea2
Terence Martin <odatnurd@gmail.com>
2024-09-04 23:48:38 -0500

    Autoload configs when projects are loaded
    This implements functionality for when projects are either created or
    loaded in windows:
    
    - If the window that just had a project loaded into it has a stored
      configuration file, immediately load it and apply the environment as
      needed.
    - If the window has no config stored, but there is exactly one envault
      config available in the project, auto-select it and load it
    - If the window has two or more configs, upon loading the project ask
      the user to select the config to use.

--- src/events.py
+++ src/events.py
@@ -1,8 +1,52 @@
 import sublime
 import sublime_plugin
 
-from .settings import ev_setting
+from .config_loader import scan_project_configs, load_and_fetch_config
 from .env_cache import fetch_env
+from .envault_data import get_envault_config, set_envault_config
+from .logging import log
+from .settings import ev_setting
+
+
+## ----------------------------------------------------------------------------
+
+
+def load_project_config(window, config):
+    """
+    Given a window and the envault configuration file that is currently set
+    within that window, fire a request to load the envault configuration for
+    that particular window.
+    """
+    log(f"doing project load fetch for {config}")
+    load_and_fetch_config(config, window)
+
+
+def check_project_for_envault_config(window):
+    """
+    Given a newly opened project window that does not yet have a selected
+    envault config, scan to see if there are any configuration files present
+    in the project.
+
+    If there are not, do nothing; if there is exactly one, then pre-select it
+    as the appropriate one. Otherwise, trigger the user to select the config
+    that they want to use in the project.
+    """
+    configs = scan_project_configs(window)
+    if not configs:
+        return
+
+    # If there is more than one configuration file, we need to prompt the
+    # user for which one to use.
+    if len(configs) != 1:
+        return window.run_command("show_overlay", {
+            "overlay": "command_palette",
+            "command": "envault_choose_config"
+            })
+
+    # There is exactly one file; set it as the currently selected envault
+    # config, then fire the load
+    set_envault_config(window, configs[0])
+    load_project_config(window, configs[0])
 
 
 ## ----------------------------------------------------------------------------
@@ -70,6 +114,25 @@ class EnvaultEventListener(sublime_plugin.EventListener):
         # We only care about build commands and watched commands
         if self.is_build(cmd, args) or self.is_watched_command(cmd):
             self.execute_env_op(window, cmd, "restore", { })
+
+
+    def on_load_project(self, window):
+        """
+        When a project loads, check to see if it has a currently defined
+        Envault configuration or not.
+
+        If it does, trigger a load to ensure that the Envault config gets set
+        for that window as appropriate.
+
+        If it does not, do a scan to see if there are any configurations
+        available, and if so choose one as needed.
+        """
+        config = get_envault_config(window)
+        if config:
+            load_project_config(window, config)
+        else:
+            check_project_for_envault_config(window)
+
 
 
 ## ----------------------------------------------------------------------------
